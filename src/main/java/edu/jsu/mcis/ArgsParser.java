c4c9c45902ee4304db63bf84e76f2c9a35fa6bfb
Wesley <jschultz@stu.jsu.edu>
2015-09-25 12:14:45 -0500

    Added three new unit test that all pass now

--- src/main/java/edu/jsu/mcis/ArgsParser.java
+++ src/main/java/edu/jsu/mcis/ArgsParser.java
@@ -5,23 +5,55 @@ import java.util.*;
 public class ArgsParser{
 	private List<String> argumentNames;
 	private List<String> argumentValues;
+	private List<String> argumentDescriptions;
 	
 	public ArgsParser()
 	{
 		argumentNames = new ArrayList<String>();
 		argumentValues = new ArrayList<String>();
+		argumentDescriptions = new ArrayList<String>();
 	}
 
 	public void addArgumentName(String name) {
 		
 		argumentNames.add(name);
 		argumentValues.add("");
+		argumentDescriptions.add("");
+	}
+	public void setValue (String name, String value){
+		int index = argumentNames.indexOf(name);
+		argumentValues.set(index,value);
+		
 	}
 	
 	public String getArgumentValue(String name) {
 		int index = argumentNames.indexOf(name);
 		return argumentValues.get(index);
 	}	
+	public void addDescription (String name, String description){
+		int index = argumentNames.indexOf(name);
+		argumentDescriptions.set(index,description);
+		
+		
+		
+	}
+	public String getDescription(String name)
+	{
+		int index = argumentNames.indexOf(name);
+		return argumentDescriptions.get(index);
+		
+		
+	}
+	public String getHelpText(){
+		String helptext = "";
+		for(int i =0; i<argumentDescriptions.size();i++)
+		{
+			helptext += argumentNames.get(i)+" "+argumentDescriptions.get(i)+"\n ";
+			
+		}
+		return helptext;
+		
+	}
 	
 	
 }
--- src/test/java/edu/jsu/mcis/ArgsParserTests.java
+++ src/test/java/edu/jsu/mcis/ArgsParserTests.java
@@ -20,6 +20,51 @@ public class ArgsParserTests{
 		assertEquals("", ap.getArgumentValue("width"));
 		assertEquals("", ap.getArgumentValue("heigth"));		
 	}
+	@Test
+	public void checkArgumentValues()
+	{
+		ap.addArgumentName("length");
+		ap.addArgumentName("width");
+		ap.addArgumentName("heigth");
+		ap.setValue("length","7");
+		ap.setValue("width","5");
+		ap.setValue("heigth","2");
+		assertEquals("7", ap.getArgumentValue("length"));
+		assertEquals("5", ap.getArgumentValue("width"));
+		assertEquals("2", ap.getArgumentValue("heigth"));
+		
+	}
+	
+	@Test 
+	public void checkArgumentDescription(){
+		ap.addArgumentName("length");
+		ap.addArgumentName("width");
+		ap.addArgumentName("heigth");
+		ap.addDescription("length","the length of the box");
+		ap.addDescription("width","the width of the box");
+		ap.addDescription("heigth","the heigth of the box");
+		assertEquals("the length of the box", ap.getDescription("length"));
+		assertEquals("the width of the box", ap.getDescription("width"));
+		assertEquals("the heigth of the box", ap.getDescription("heigth"));
+		
+	}
+	@Test
+	public void checkIfDashHPrintsTheDescriptions()
+	{
+		ap.addArgumentName("length");
+		ap.addArgumentName("width");
+		ap.addArgumentName("heigth");
+		ap.addDescription("length","the length of the box");
+		ap.addDescription("width","the width of the box");
+		ap.addDescription("heigth","the heigth of the box");
+		assertEquals("length the length of the box\n width the width of the box\n heigth the heigth of the box\n ",
+					ap.getHelpText());
+		
+		
+	}
+	@Test
+	
+	
 	//@Test
 	public void checkIfArgsParserGetsArgs(){
 		
