63fa10e9cf6d334e3ca46d375248e3c16c368146
Wesley <jschultz@stu.jsu.edu>
2015-10-01 17:09:16 -0500

    same

--- acceptance/ArgsParserKeywords.java
+++ acceptance/ArgsParserKeywords.java
@@ -3,38 +3,68 @@ import edu.jsu.mcis.*;
 public class ArgsParserKeywords {
 	
 	private ArgsParser ap;
-	/*
+	
 	public void startVolumeCacluatorWithArguments(String[] args) {
 		ap = new ArgsParser();
-		ap.addArgument("length");
-		ap.addArgument("width");
-		...
+		ap.addProgram("VolumeCalculator","Calculate the volume of a box");
+		ap.addArgument("length","the length of the box");
+		ap.addArgument("width","the width of the box");
+		ap.addArgument("heigth","the heigth of the box");
 		ap.parseValues(args);
 		
 	}
 	
 	public String getLength() {
-		return p.getArgValue("length");
+		return ap.getStringValue("length");
+	}
+	public String getWidth() {
+		return ap.getStringValue("width");
+	}
+	public String getHeight() {
+		return ap.getStringValue("height");
+	}
+	public String getPet() {
+		return ap.getStringValue("pet");
+	}
+	public String getNumber() {
+		return ap.getStringValue("number");
+	}
+	public String getRainy() {
+		return ap.getStringValue("rainy");
+	}
+	public String getBathrooms() {
+		return ap.getStringValue("bathrooms");
 	}
 	
-	...
-	
 	public String getProgramOutput() {
+		if(getLength().equals("-h")){
+			return ap.getHelpMessage();
+		}
+		else{
 		float len = Float.parseFloat(getLength());
-		float wid = 
-		float hei = 
-		return String.valueOf(len * wid * hei);
+		float wid = Float.parseFloat(getWidth());
+		float hei = Float.parseFloat(getHeigth());
+		return String.valueOf(len * wid * hei);}
 	}
 	
 	public void startAbsurdProgramWithArguments() {
+		ap = new ArgsParser();
+		ap.addProgram("VolumeCalculator","");
+		ap.addArgument("pet","");
+		ap.addArgument("number","");
+		ap.addArgument("rainy","");
+		ap.addArgument("bathrooms","");
+		ap.parseValues(args);
+		
 		
 	}
-	*/
+	
 	public void startProgramWithArguments(String[] args) {
 		ap = new ArgsParser();
 		ap.addProgram("VolumeCalculator","Calculate the volume of a box");
 		ap.addArgument("length","the length of the box");
 		ap.addArgument("width","the width of the box");
 		ap.addArgument("heigth","the heigth of the box");
+		ap.parseValues(args);
 	}
 }
