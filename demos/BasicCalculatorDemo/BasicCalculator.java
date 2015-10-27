71f5bf2b16fc582a338c1dc7a8cfd8f04a0f310a
WhitneyW12 <whitneybwood12@gmail.com>
2015-10-27 16:36:54 -0500

    finished demo with code we have except for string to double error

--- demos/BasicCalculatorDemo/BasicCalculator.java
+++ demos/BasicCalculatorDemo/BasicCalculator.java
@@ -2,12 +2,32 @@ import edu.jsu.mcis.*;
 
 public class BasicCalculator{
 	public static void main(String[] args){
+		//args = new String[] {"--add","8","7"};
 		ArgsParser ap = new ArgsParser();
 		ap.addProgram("BasicCalculator","Add, subtract, and multiply numbers.");
 		ap.addArgument("firstNumber","The first number to do calculations on",Argument.Type.DOUBLE);
 		ap.addArgument("secondNumber","The second number to do calculations on",Argument.Type.DOUBLE);
-		ap.addNamedArgument("--add","Adds the numbers",Argument.Type.BOOLEAN,1,false);
-		ap.addNamedArgument("--subtract","Subtracts the numbers",Argument.Type.BOOLEAN,1,false);
-		ap.addNamedArgument("--multiply","Multiplies the numbers",Argument.Type.BOOLEAN,1,false);
+		ap.addNamedArgument("--add","Adds the numbers",Argument.Type.BOOLEAN,2,false);
+		ap.addNamedArgument("--subtract","Subtracts the numbers",Argument.Type.BOOLEAN,2,false);
+		ap.addNamedArgument("--multiply","Multiplies the numbers",Argument.Type.BOOLEAN,2,false);
+		ap.parseValues(args);
+		double firstNumber = ap.getValue("firstNumber");
+		double secondNumber = ap.getValue("secondNumber");
+		boolean add = ap.getValue("--add");
+		boolean subtract = ap.getValue("--subtract");
+		boolean multiply = ap.getValue("--multiply");
+		System.out.print("The answer is: ");
+		if (add){
+			System.out.print(firstNumber + secondNumber);
+		}
+		else if (subtract){
+			System.out.print(firstNumber - secondNumber);
+		}
+		else if (multiply){
+			System.out.print(firstNumber * secondNumber);
+		}
+		else{
+			System.out.println("No action was selected. Type --help for more information.");
+		}
 	}
 }
