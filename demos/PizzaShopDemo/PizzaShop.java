5291e509cadb6a47b089a4655e41a5f49f3296bf
WhitneyW12 <whitneybwood12@gmail.com>
2015-11-03 16:52:07 -0600

    added new pizza demo

---
+++ demos/PizzaShopDemo/PizzaShop.java
@@ -0,0 +1,13 @@
+import edu.jsu.mcis.*;
+
+public class PizzaShop{
+	public static void main(String[] args){
+		ArgsParser ap = new ArgsParser();
+		ap.addProgram("PizzaShop","Order a pizza.");
+		
+		
+		
+		
+		
+	}
+}
