9ec5b55e6d9bd81a59dc7abfb10468619a72f964
WhitneyW12 <whitneybwood12@gmail.com>
2015-11-09 12:02:10 -0600

    dashes fixed and start of new demo

--- demos/BasicCalculatorDemo/BasicCalculator.java
+++ demos/BasicCalculatorDemo/BasicCalculator.java
@@ -2,20 +2,19 @@ import edu.jsu.mcis.*;
 
 public class BasicCalculator{
 	public static void main(String[] args){
-		//args = new String[] {"--add","8","7"};
 		ArgsParser ap = new ArgsParser();
 		ap.addProgram("BasicCalculator","Add, subtract, and multiply numbers.");
 		ap.addPositionalArgument("firstNumber","The first number to do calculations on",Argument.Type.DOUBLE);
 		ap.addPositionalArgument("secondNumber","The second number to do calculations on",Argument.Type.DOUBLE);
-		ap.addNamedArgument("--add","Adds the numbers",Argument.Type.BOOLEAN,"false","-a");
-		ap.addNamedArgument("--subtract","Subtracts the numbers",Argument.Type.BOOLEAN,"false","-s");
-		ap.addNamedArgument("--multiply","Multiplies the numbers",Argument.Type.BOOLEAN,"false","-m");
+		ap.addNamedArgument("add","Adds the numbers",Argument.Type.BOOLEAN,"false","a");
+		ap.addNamedArgument("subtract","Subtracts the numbers",Argument.Type.BOOLEAN,"false","s");
+		ap.addNamedArgument("multiply","Multiplies the numbers",Argument.Type.BOOLEAN,"false","m");
 		ap.parseValues(args);
 		double firstNumber = ap.getValue("firstNumber");
 		double secondNumber = ap.getValue("secondNumber");
-		boolean add = ap.getValue("--add");
-		boolean subtract = ap.getValue("--subtract");
-		boolean multiply = ap.getValue("--multiply");
+		boolean add = ap.getValue("add");
+		boolean subtract = ap.getValue("subtract");
+		boolean multiply = ap.getValue("multiply");
 		System.out.print("The answer is: ");
 		if (add){
 			System.out.print(firstNumber + secondNumber);
