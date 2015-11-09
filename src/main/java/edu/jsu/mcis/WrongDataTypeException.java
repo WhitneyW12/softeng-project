4aef97c83b30cb8704fb288fc5c3ee0d1afec4b9
Wesley <jschultz@stu.jsu.edu>
2015-11-09 11:55:11 -0600

    took out unnessasary exceptions

--- src/main/java/edu/jsu/mcis/WrongDataTypeException.java
+++
@@ -1,9 +0,0 @@
-package edu.jsu.mcis;
-import java.util.*;
-
-public class WrongDataTypeException extends RuntimeException{
-
-	public WrongDataTypeException(String errorMessage){
-		super(errorMessage);
-	}
-}
