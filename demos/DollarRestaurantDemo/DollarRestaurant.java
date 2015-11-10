cfe864c23f36a2371f5dd1cad0a557c6e8e5e05e
WhitneyW12 <whitneybwood12@gmail.com>
2015-11-10 17:05:06 -0600

    3rd demo almost finished

--- demos/DollarRestaurantDemo/DollarRestaurant.java
+++ demos/DollarRestaurantDemo/DollarRestaurant.java
@@ -4,6 +4,22 @@ public class DollarRestaurant{
 	public static void main(String[] args){
 		ArgsParser ap = new ArgsParser();
 		ap.addProgram("DollarRestaurant","All food is a dollar!");
+		ap.addPositionalArgument("entree","What would you like as your entree?",Argument.Type.STRING);
 		
+		ap.addNamedArgument("appetizer","What would you like as an appetizer?",Argument.Type.STRING,"no appetizer","a");
+		ap.addNamedArgument("dessert","What would you like for dessert?",Argument.Type.STRING,"no dessert","d");
+		ap.parseValues(args);
+		double amount = 0;
+		double tip = 0;
+		double total = 0;
+		if(ap.getValue("appetizer") != "no appetizer"){
+			amount++;
+		}
+		if(ap.getValue("dessert") != "no dessert"){
+			amount++;
+		}
+		tip = .10 * amount;
+		total = amount + tip;
+		System.out.println("Your total is $" + String.format("%.2f", total) +".");
 	}
 }
