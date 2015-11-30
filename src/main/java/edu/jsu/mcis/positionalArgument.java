c28c4ee787afb10d106d913a188967f61dcf4cbd
Wesley <jschultz@stu.jsu.edu>
2015-11-30 12:06:54 -0600

    fixed positional argument

--- src/main/java/edu/jsu/mcis/positionalArgument.java
+++ src/main/java/edu/jsu/mcis/positionalArgument.java
@@ -1,5 +1,13 @@
 package edu.jsu.mcis;
-
+/**
+ *This program creates a positionalArgumentclass that extends Argument class.
+ *
+ *@author BabaTunde Idumu
+ *@author Michael Quattrochi
+ *@author Wesley Schultz
+ *@author James Thomas
+ *@author Whitney Wood
+ */
 public class positionalArgument extends Argument{
 	public positionalArgument(Type type, String description){
 		super(type, description);
