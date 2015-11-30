bbf8a2e74666dee075b649a3d4b4a37a1b245d1d
Wesley <jschultz@stu.jsu.edu>
2015-11-30 11:39:43 -0600

    Revert "Started documentation on namedArgument"
    This reverts commit 81e23d92f375f1be0804f1b28c82639f331a91e2.

--- src/main/java/edu/jsu/mcis/namedArgument.java
+++ src/main/java/edu/jsu/mcis/namedArgument.java
@@ -1,22 +1,8 @@
-/**
-*<p>
-*This program creates a namedArgument class that extends Argument class.
- *
- *@author BabaTunde Idumu
- *@author Michael Quattrochi
- *@author Wesley Schultz
- *@author James Thomas
- *@author Whitney Wood
-*/
 package edu.jsu.mcis;
 public class namedArgument extends Argument{
 	
 
 	public namedArgument(Type type, String description){
-		/**
-		*<p>
-		*The super constructor returns the type and description.
-		*/
 		 super(type,description);
 	 }
 	 public String getDescription(){
