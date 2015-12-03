8261d03a4067de63516bb5e3affecce1b2727050
Wesley <jschultz@stu.jsu.edu>
2015-12-03 10:12:54 -0600

    xml parse complete

--- src/main/java/edu/jsu/mcis/readWriteXML.java
+++ src/main/java/edu/jsu/mcis/readWriteXML.java
@@ -1,13 +1,4 @@
-/**
- * <p> 
- * Creates a new class to parse an XML file.
- * Class extends from DefaultHandler superclass.
- * @author BabaTunde Idumu
- * @author Michael Quattrochi
- * author Wesley Schultz
- * @author James Thomas
- * @author Whitney Wood
-*/
+package edu.jsu.mcis;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -19,9 +10,15 @@ import java.io.*;
 import edu.jsu.mcis.*;
 
 /**
+ * <p> 
  * Creates a new class to parse an XML file.
  * Class extends from DefaultHandler superclass.
- */
+ * @author BabaTunde Idumu
+ * @author Michael Quattrochi
+ * author Wesley Schultz
+ * @author James Thomas
+ * @author Whitney Wood
+*/
 
 public class readWriteXML extends DefaultHandler{
 	
@@ -41,10 +38,7 @@ public class readWriteXML extends DefaultHandler{
 	private String ashorthand;
 	private String adefault;
 	private Argument.Type atype;
-	
-	/**
-	 * public class constructor
-	 */
+
 	public readWriteXML(){
 		nameAttributes= new StringBuilder();
 		typeAttributes=new StringBuilder();
@@ -56,17 +50,16 @@ public class readWriteXML extends DefaultHandler{
 		description = false;
 		defaults = false;
 		shorthand = false;
-		ap =	new ArgParser();	
+		ap =	new ArgsParser();	
 		aname="";
 		adescription="";
 		ashorthand="";
 		atype= Argument.Type.STRING;
 	}
-	
-	/**
+/**
 	 * New instance of DefaultHandler class is created called "handler".
 	 */
-	DefaultHandler handler = new DefaultHandler(){
+	public DefaultHandler handler = new DefaultHandler(){
 	/**
 	 * <p>
 	 * This method is used at the beginning of every element in the XML
@@ -91,33 +84,34 @@ public class readWriteXML extends DefaultHandler{
 	public void startElement(String uri, String localName,String qName, 
                 Attributes attributes) throws SAXException {
 
-		System.out.println("Start Element :" + qName);
 		if(qName.equals("arguments")){
 			ap.addProgram(aname,adescription);
 		}
 
-		else if (qName.equalsIgnoreCase("name")) {
-			name = true;	
+		if (qName.equalsIgnoreCase("name")) {
+			name = true;
+			
 		}
 
-		else if (qName.equalsIgnoreCase("type")) {
+		if (qName.equalsIgnoreCase("type")) {
 			type = true;
 			
 		}
-		else if (qName.equalsIgnoreCase("description")) {
+
+		if (qName.equalsIgnoreCase("description")) {
 			description = true;
 			 
 		}
-		else if (qName.equalsIgnoreCase("defaults")) {
+		
+		if (qName.equalsIgnoreCase("default")) {
 			defaults = true;
 		}
-		else (qName.equalsIgnoreCase("shorthand")) {
+		if (qName.equalsIgnoreCase("shorthand")) {
 			shorthand = true;
 		}
 			
 	}
-	
-	/**
+		/**
 	 * <p>
 	 * This method is notified when the end of an element is reached. Does nothing,
 	 * by default. Specifies whether element is a positional or named argument.
@@ -138,10 +132,9 @@ public class readWriteXML extends DefaultHandler{
 		if(qName.equals("positionalArgument")){
 			ap.addPositionalArgument(aname,adescription,atype);
 		}
-		else(qName.equals("namedArgument")){
+		if(qName.equals("namedArgument")){
 			ap.addNamedArgument(aname,adescription,atype,adefault,ashorthand);
 		}
-		System.out.println("End Element :" + qName);
 		
 
 	}
@@ -163,76 +156,58 @@ public class readWriteXML extends DefaultHandler{
 
 		if (name) {
 			aname=new String(ch, start, length);
-			System.out.println("name : " + new String(ch, start, length));
 			name = false;
 			
 		}
 
 		if (type) {
-			if(new String(ch, start, length).equals("DOUBLE")){
+			if(new String(ch, start, length).equals("STRING")){
+				atype=Argument.Type.STRING;
+			}
+			else if(new String(ch, start, length).equals("DOUBLE")){
 				atype=Argument.Type.DOUBLE;
 			}
 			else if (new String(ch, start, length).equals("INTEGER")){
-				atype=ARGUMENT.Type.INTEGER;
+				atype=Argument.Type.INTEGER;
 			}
-			else{
-				atype=ARGUMENT.Type.BOOLEAN;
+			else {
+				atype=Argument.Type.BOOLEAN;
 			}
-			System.out.println("type : " + new String(ch, start, length));
 			type = false;
 		}
 
 		if (description) {
 			adescription=new String(ch, start, length);
-			System.out.println("description : " + new String(ch, start, length));
+		
 			description = false;
 		}
 		
 		if (defaults) {
 			adefault=new String(ch, start, length);
-			System.out.println("defaults : " + new String(ch, start, length));
 			defaults = false;
 		}
 		
 		if (shorthand) {
 			ashorthand= new String(ch, start, length);
-			System.out.println("shorthand : " + new String(ch, start, length));
 			shorthand= false;
 		}
 	}
 	};
 	
-	/**
-	 * <p>
-	 * This method creates and returns a new instance of the ArgsParser class.
-	 * @return ap is an instance of ArgsParser.
-	 */
-	public ArgParser getArgParser(){
+	private ArgsParser getArgsParser(){
 		return ap;
 	}
-	
-	/**
-	 * <p>
-	 * This is the main method of the public class.
-	 * Creates a new instance of the readWriteXML class.
-	 */
-    public static void main(String argv[]) {
-		readWriteXML rw = new readWriteXML();
-	    try{
-			InputStream xmlInput = new FileInputStream("readXML.xsl");
-		    SAXParserFactory factory = SAXParserFactory.newInstance();
-		    SAXParser saxParser = factory.newSAXParser();
-
-		    saxParser.parse(xmlInput,rw.handler);
-
-		    //DefaultHandler handler = new DefaultHandler() {
-		}catch (Exception e) {
+	public ArgsParser parseXML(String filepath){
+		try {
+			InputStream xmlInput = new FileInputStream(filepath);
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser saxParser = factory.newSAXParser();
+			saxParser.parse(xmlInput,handler);
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println(rw.getNames());
-		System.out.println(rw.getType());
-		  
-   }
-
+		return getArgsParser();
+	}
 
 }
